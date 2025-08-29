77e59fc7e9cd54f5a9d1d05b515ecff5d22b3b67
Umiar Iusupov <umiarka@gmail.com>
2025-08-29 01:26:32 +0100

    sidebar, header & theme toggle

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,117 @@
+import { Button } from '@/components/ui/button';
+import * as React from 'react';
+import { PowerIcon, Wallet } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+
+const data = {
+  navMain: [
+    {
+      title: 'Home',
+      url: '#',
+      items: [
+        {
+          title: 'Home Sub 1',
+          url: '#',
+          isActive: true,
+        },
+        {
+          title: 'Home Sub 2',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Receipts',
+      url: '#',
+      items: [
+        {
+          title: 'Receipts Sub 1',
+          url: '#',
+        },
+        {
+          title: 'Receipts Sub 2',
+          url: '#',
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Wallet className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">Expenses</span>
+                  <span className="">management</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map(item => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map(item => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <form
+          action={async () => {
+            'use server';
+            console.log('Sign out');
+            // await signOut({ redirectTo: '/' });
+          }}
+        >
+          <Button variant="ghost" className="cursor-pointer w-full">
+            <PowerIcon className="size-4" />
+            <div>Sign Out</div>
+          </Button>
+        </form>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
